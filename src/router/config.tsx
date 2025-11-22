bd5a4905eb08a16bb747b203e6585d882ec41df2
mcoficinadaimagem-web <mcoficinadaimagem@gmail.com>
2025-11-22 14:33:08 -0300

    Update config.tsx

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -1,11 +1,16 @@
 import type { RouteObject } from "react-router-dom";
 import NotFound from "../pages/NotFound";
 import Home from "../pages/home/page";
+import PersonalOrganizerLanding from "../pages/personal-organizer/page";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/personal-organizer",
+    element: <PersonalOrganizerLanding />,
   },
   {
     path: "*",
